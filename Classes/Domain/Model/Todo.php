c4ebe7508cb0252067b60438646dc74f06dc4ec7
Marc Neuhaus <mneuhaus@famelo.com>
2011-11-17 15:09:46 +0100

    Added Todo as item

--- Classes/Domain/Model/Todo.php
+++ Classes/Domain/Model/Todo.php
@@ -6,7 +6,9 @@ namespace AdminDemo\Domain\Model;
  *                                                                        *
  *                                                                        */
 
+use Doctrine\ORM\Mapping as ORM;
 use TYPO3\FLOW3\Annotations as FLOW3;
+use Admin\Annotations as Admin;
 
 /**
  * A Todo
@@ -15,6 +17,25 @@ use TYPO3\FLOW3\Annotations as FLOW3;
  * @FLOW3\Entity
  */
 class Todo {
+	/**
+	 * @var string
+	 * @FLOW3\Validate(type="String")
+	 * @FLOW3\Validate(type="NotEmpty")
+	 */
+	protected $name;
+	
+	/**
+	 * @var string
+	 * @Admin\Widget("Textarea")
+	 */
+	protected $description;
+	
+	/**
+	 * @var \Doctrine\Common\Collections\ArrayCollection<\AdminDemo\Domain\Model\Project>
+	 * @ORM\ManyToMany
+	 * @Admin\Ignore("list")
+	 */
+	protected $projects;
 	
 }
 ?>
